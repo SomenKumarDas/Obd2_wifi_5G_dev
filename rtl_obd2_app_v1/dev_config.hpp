bfedac310c3f9c764e297e07a49c860d020bfec1
SomenKumarDas <somen.k.das2@gmail.com>
2022-05-04 12:00:22 +0530

    add: Custom logging

--- rtl_obd2_app_v1/dev_config.hpp
+++ rtl_obd2_app_v1/dev_config.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #include "WiFi.h"
+#include "dev_log.hpp"
 
 /*** Macro Definetions ***/
 #define UART_LOG Serial
@@ -9,7 +10,13 @@
 
 #define STATUS_FN(func, ret) if((func) == false) {ret}
 
+#define log_v(format, ...) printf(APP_LOG_FORMAT(V, format), ##__VA_ARGS__)
+#define log_d(format, ...) printf(APP_LOG_FORMAT(D, format), ##__VA_ARGS__)
+#define log_i(format, ...) printf(APP_LOG_FORMAT(I, format), ##__VA_ARGS__)
+#define log_w(format, ...) printf(APP_LOG_FORMAT_LINE(W, format), ##__VA_ARGS__)
+#define log_e(format, ...) printf(APP_LOG_FORMAT_LINE(E, format), ##__VA_ARGS__)
 
+#define sleep(x) vTaskDelay(pdMS_TO_TICKS(x));
 
 /**
  * @brief intialize all the device hardwares and Peripherals,
